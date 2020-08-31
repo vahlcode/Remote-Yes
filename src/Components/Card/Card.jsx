563705b0969e3c44d6924cba83230f2349c3bb27
Valentine Elum <valentineelum@gmail.com>
2020-08-31 18:47:07 +0100

    Fixed blank job types on some jobs.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -120,7 +120,7 @@ class Card extends Component {
                                     <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                                 </svg>
-                                {job.job_type && job.job_type.replace(/["_"]/g, " ").toUpperCase()}
+                                {job.job_type && job.job_type ? job.job_type.replace(/["_"]/g, " ").toUpperCase() : "Not specified"}
                             </span>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-dollar-sign">
