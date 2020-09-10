729ab89b2c27df8ec5ebef4f176291cc11bc9469
Valentine Elum <valentineelum@gmail.com>
2020-09-11 00:55:08 +0100

    Fix job limit

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -85,7 +85,7 @@ class Index extends Component {
     if (localStorage.getItem("signed")) {
       const signal = this.signal
       
-      fetch(`https://shielded-sands-39483.herokuapp.com/${LINK}&limit=50`, {signal})
+      fetch(`https://shielded-sands-39483.herokuapp.com/${LINK}&limit=100`, {signal})
       .then(response => response.json())
       .then(jobs => { 
           this.setState({...this.state, jobs: jobs})
